a6efd769840a6fa6af4cd25b0a5b20c1f75337b2
Simon T. <simtho0624@gmail.com>
2025-02-04 12:57:11 +0100

    Add vertical rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,6 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * movePerSec * verticalInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
