a91bf0dab7b5c4e588dde600279a93ec79822c84
Simon T. <simtho0624@gmail.com>
2025-02-04 13:04:52 +0100

    Make variabler private

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Move x times per second
-    public float movePerSec = 10.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float verticalInput;
+    private float movePerSec = 10.0f;
+    private float turnSpeed = 10.0f;
+    private float horizontalInput;
+    private float verticalInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        // We'll move the vehicle forward
+        // We move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * movePerSec * verticalInput);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
