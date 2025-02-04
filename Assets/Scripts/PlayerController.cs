f81ed03e72e23f44769fcfa34fac8fe98f032bf9
Simon T. <simtho0624@gmail.com>
2025-02-04 12:53:02 +0100

    Add horizontal and vertical input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerController : MonoBehaviour
     // Move x times per second
     public float movePerSec = 10.0f;
     public float turnSpeed;
+    public float horizontalInput;
+    public float verticalInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,8 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * movePerSec);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * movePerSec * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
